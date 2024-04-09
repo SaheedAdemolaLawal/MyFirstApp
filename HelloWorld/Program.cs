59178edeb648d1b9959e9763d5642e0e1f05267f
Azeezah Lawal <azeezah.a.lawal@gmail.com>
2024-04-09 19:09:22 +0100

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿
+string Colour, Pluralnoun, Celebrity;
+
+
+Console.Write("Enter a Colour: ");
+Colour = Console.ReadLine();
+
+Console.Write(" Enter Pluralnoun: ");
+Pluralnoun = Console.ReadLine();
+
+Console.Write("Enter a Celebrity: ");
+Celebrity = Console.ReadLine();
+
+Console.WriteLine("Roses are" + Colour);
+Console.WriteLine(Pluralnoun +  "are blues");
+Console.WriteLine("I love" + Celebrity);
+
+
+
+
+
+
+
+
+
+Console.ReadLine();
